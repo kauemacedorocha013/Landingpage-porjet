2dde58b2ed4e7a9f8376d75367928fc85d9752a4
mc 2k da mata <kauemacedo013@gmail.com>
2025-07-04 12:12:28 -0300

    Atualização da landingpage (recomeçando)

--- js/scrpit.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function AbreFechaMenu() {
-  const hamburger = document.querySelector(".icon-hamburger");
-  const navLinks = document.querySelector(".navbar-container-links");
-
-  hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-  });
-});
