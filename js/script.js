dbd4486114c02a2728dffbf62379f288d2174675
mc 2k da mata <kauemacedo013@gmail.com>
2025-08-12 19:22:28 -0300

    atualização: resolvido o problema de responsividade do menu, seguindo com a construção do projeto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById("n-icon-menu");
   const navLinks = document.querySelector(".c-menu");
+  const getStartedButton = document.getElementById("bt-comecar");
 
-  hamburger.addEventListener("click", () => {navLinks.classList.toggle("active");});
+  hamburger.addEventListener("click", () => {navLinks.classList.toggle("active");
+    getStartedButton.classList.toggle("active");
+  });
+  
 });
